b1332cb95139a441faf829499f9641f709121435
Brandon Oakley <brandon@Brandons-MacBook-Pro.local>
2021-01-04 23:16:02 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import Entype from 'react-native-vector-icons/Entypo';
+import HomeScreen from './src/screens/Home/index';
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <HomeScreen />
+      </SafeAreaView>
+    </>
+  );
+};
+
+
+
+export default App;
