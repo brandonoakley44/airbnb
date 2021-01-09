282ae2e2184541172de40b677016ad06494e2ae9
Brandon Oakley <brandon@Brandons-MacBook-Pro.local>
2021-01-09 19:01:00 -0400

    Carousel list

--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, useWindowDimensions } from 'react-native';
 import MapView, { Marker , PROVIDER_GOOGLE} from 'react-native-maps';
 import CustomMarker from '../../components/CustomMarker';
 import PostCarouselItem from '../../components/PostCarouselItem';
@@ -8,6 +8,8 @@ import places from '../../../assets/data/feed';
 const SearchResultsMap = (props) => {
 
     const [selectedPlaceId, setSelectedPlaceId] = useState(null);
+
+    const width = useWindowDimensions().width;
 
     return (
         <View style={{ width: '100%', height: '100%' }} >
@@ -31,8 +33,16 @@ const SearchResultsMap = (props) => {
               )) }
             </MapView>
 
-            <View style={{position: 'absolute', bottom: 40}}>
-                <PostCarouselItem post={places[0]} />
+            <View style={{position: 'absolute', bottom: 10}}>
+                <FlatList
+                    data={places}
+                    renderItem={({item}) => <PostCarouselItem post={item} />}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    snapToInterval={ width - 60 }
+                    snapToAlignment={"center"}
+                    decelerationRate={"fast"}
+                />
             </View>
 
 
