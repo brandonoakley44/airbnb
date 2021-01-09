3141957e3b10725e3d92227415eb861492cd2804
Brandon Oakley <brandon@Brandons-MacBook-Pro.local>
2021-01-09 18:51:34 -0400

    Carousel Item

--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import MapView, { Marker , PROVIDER_GOOGLE} from 'react-native-maps';
 import CustomMarker from '../../components/CustomMarker';
-
+import PostCarouselItem from '../../components/PostCarouselItem';
 import places from '../../../assets/data/feed';
 
 const SearchResultsMap = (props) => {
@@ -30,6 +30,12 @@ const SearchResultsMap = (props) => {
                   />    
               )) }
             </MapView>
+
+            <View style={{position: 'absolute', bottom: 40}}>
+                <PostCarouselItem post={places[0]} />
+            </View>
+
+
         </View>
     );
 }
