10e6ddb41d28be285252139f5d40e27128d8e964
Brandon Oakley <brandon@Brandons-MacBook-Pro.local>
2021-01-08 23:26:46 -0400

    Top tab bars

--- src/navigation/ExploreNavigator.js
+++ src/navigation/ExploreNavigator.js
@@ -3,6 +3,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/Home';
 import SearchResultsScreen from '../screens/SearchResults';
 
+import SearchResultsTabNavigator from './SearchResultsTabNavigator';
+
 const Stack = createStackNavigator();
 
 const Router = (props) => {
@@ -20,7 +22,7 @@ const Router = (props) => {
 
         <Stack.Screen
             name={'SearchResults'}
-            component={SearchResultsScreen}
+            component={SearchResultsTabNavigator}
             options={{
                 title: "Search your destination"
             }}
